926061f7209649c2496c298f94216d63b406349f
chilekampalligangadineshreddy-bit <chilekampalligangadineshreddy@gmail.com>
2026-07-18 16:36:45 +0530

    Add files via upload

---
+++ files/scripts/main.py
@@ -0,0 +1,7 @@
+from scenario1_overall_trends import overall_trends
+from scenario2_regional_variation import regional_variation
+from scenario3_lockdown_recovery import lockdown_recovery
+
+overall_trends()
+regional_variation()
+lockdown_recovery()
